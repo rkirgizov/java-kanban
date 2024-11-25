35d2ea93c3b1688908942277265e3dd3001e128d
rkirgizov <r.kirgizov@live.com>
2024-11-25 15:28:03 +0300

    Финальный проект спринта 6 - небольшие исправления по стилю в main (ошибка при проверке пул-реквеста) 2

--- src/com/rkirgizov/practicum/service/TaskManager.java
+++ src/com/rkirgizov/practicum/service/TaskManager.java
@@ -10,25 +10,41 @@ public interface TaskManager {
     List<Task> getHistory();
 
     List<Task> getAllTasks();
+
     void removeAllTasks();
+
     Task getTaskById(int id);
+
     void createTask(Task task);
+
     void updateTask(Task task);
+
     void removeTask(Task task);
 
     List<SubTask> getAllSubtasks();
+
     void removeAllSubTasks();
+
     SubTask getSubtaskById(int id);
+
     void createSubTask(SubTask subTask);
+
     void updateSubtask(SubTask subTask);
+
     void removeSubTask(SubTask subTask);
 
     List<Epic> getAllEpics();
+
     void removeAllEpics();
+
     Epic getEpicById(int id);
+
     void createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void removeEpic(Epic epic);
+
     List<SubTask> getAllSubtasksOfEpic(int id);
 
 }
