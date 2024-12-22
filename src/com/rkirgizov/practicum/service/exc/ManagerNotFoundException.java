31802ecbd59eda9af8538c20d8f0d1e49dba1c20
rkirgizov <r.kirgizov@live.com>
2024-12-23 02:03:25 +0330

    Финальный проект спринта 9

--- src/com/rkirgizov/practicum/service/exc/ManagerNotFoundException.java
+++ src/com/rkirgizov/practicum/service/exc/ManagerNotFoundException.java
@@ -1,5 +1,7 @@
 package com.rkirgizov.practicum.service.exc;
 
 public class ManagerNotFoundException extends RuntimeException {
-    public ManagerNotFoundException(String message) { super(message); }
+    public ManagerNotFoundException(String message) {
+        super(message);
+    }
 }
