2aa2db72ba14934cf8fe3c8998222e9526e70d02
rkirgizov <r.kirgizov@live.com>
2024-12-23 02:04:44 +0330

    Финальный проект спринта 9

--- src/com/rkirgizov/practicum/http/adapt/LocalDateTimeAdapter.java
+++ src/com/rkirgizov/practicum/http/adapt/LocalDateTimeAdapter.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 //    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
         if (localDateTime == null) {
